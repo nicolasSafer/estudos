c1e3baf2aff3fec6a8a9436967871059b4821505
Nícolas <93827543+nicolasSafer@users.noreply.github.com>
2024-06-07 16:02:48 -0300

    Update tarefa_controller.php

--- tarefa_controller.php
+++ tarefa_controller.php
@@ -1,5 +1,5 @@
 <?php
 //a pasta app tarefas está fora do htdocs ou seja fora do online
-require_once '../../../../../app_tarefas/tarefa_controller.php';
+require_once '../../../../../app_tarefas/tarefa_controller_private.php';
 
 ?>
